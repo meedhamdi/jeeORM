e36cf38ad2d5f18ece2d16ef834e4eac8f6a273b
Mohammed El Hamdi <elhamdi56@gmail.com>
2022-07-07 02:56:31 +0200

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,36 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Test {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("template-persistence-unit");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		boolean transactionOK = false;
+		try {
+			Emp emp = new Emp();
+			emp.setName("Med");
+			emp.setJob("Data engineer");
+			// insert emp to database
+			em.persist(emp);
+			// remove emp from database
+			//em.remove(emp);
+			transactionOK = true;
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		finally {
+			
+			if (transactionOK) {
+				em.getTransaction().commit();
+			} else {
+				em.getTransaction().rollback();
+			}
+			
+			em.close();
+		}
+	}
+}
